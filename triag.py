eee2adc716d38a0dc3afc787efcced46df106ca1
Olga <6402303@mail.ru>
2023-10-28 00:19:07 +0300

    Add DZ

---
+++ triag.py
@@ -0,0 +1,11 @@
+Задание с треугольником
+height_triangle=6
+height_counter=3
+
+while height_counter<=height_triangle:
+   n=0
+   while (n<height_counter):
+      print('*',end="")
+      n=n+1
+   print("")
+   height_counter=height_counter+1
